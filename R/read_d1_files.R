c5a165c618b8d54fde2a00ffd1295a7a43d25534
Julien Brun <brun.julien@gmail.com>
2018-06-20 16:47:53 -0700

    switch to folder path and more cases handling

--- R/read_d1_files.R
+++ R/read_d1_files.R
@@ -1,18 +1,29 @@
-read_d1_files <- function(files, fnc = "read_csv") {
+read_d1_files <- function(folder_path, fnc = "read_csv") {
+  
+  files <- list.files(folder_path, full.names = TRUE)
+  # files <- files[!grepl(pattern='full_metadata.xml', files)]
+  filename <- files[grepl(pattern='__summary_metadata.csv', files)]
+  filename <- gsub(pattern='__summary_metadata.csv','', basename(filename), fixed = TRUE)
+  
+  if (sum(filename == tools::file_path_sans_ext(basename(files))) > 1 ){
+    stop("You have multiple files named ", filename)
+    }
+  
   data_meta <- map(files, function(x){
     if (grepl("[^_]+_metadata(?=\\.csv)", basename(x), perl = TRUE)) {
       read_csv(x)
-    } else {
+    } else if (tools::file_path_sans_ext(basename(x)) == filename) {
       eval(parse(text = paste0(fnc, '("', x, '")')))
     }
   }) 
   data_meta_names <- map(files, function(x){
     if (grepl("[^_]+_metadata(?=\\.csv)", basename(x), perl = TRUE)) {
       stringr::str_extract(basename(x), "[^_]+_metadata(?=\\.csv)")
-    } else {
+    } else if (tools::file_path_sans_ext(basename(x)) == filename){
       "data"
     }
   }) 
-  data_meta <- setNames(data_meta, data_meta_names)
+  data_meta <- setNames(data_meta, data_meta_names) %>%
+    compact()
   return(data_meta)
 }
