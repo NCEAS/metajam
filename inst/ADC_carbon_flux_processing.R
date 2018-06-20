7cb0fc24881756472eaf9fde07ec00e44ef4b1c3
Julien Brun <brun.julien@gmail.com>
2018-06-19 17:23:45 -0700

    start reading data and metada

--- inst/ADC_carbon_flux_processing.R
+++ inst/ADC_carbon_flux_processing.R
@@ -4,12 +4,16 @@
 library(metajam)
 
 library(tidyverse)
+library(googledrive)
+library(googlesheets)
+library(janitor)
+library(skimr)
 
 
 ###### CONSTANTS ###### 
 
 # Where we download the data from D1
-data_folder <- "Data"
+data_folder <- "~/Desktop/DataADC"
 
 # ADC permafrost data sets listing (thank you Steven!)
 adc_data_listing <- "ADC_permafrost_related_datasets"
@@ -30,6 +34,29 @@ data_listing <- # Getting the test datasets listing
 ### Download the data and metadata ----
 
 # batch download the datasets
-map2(data_listing$pid, data_folder, download_D1Data)
+map2(data_listing$pid, data_folder, download_d1_data)
+
+
+### Read data files back into R as a named list ----
+
+# List the files
+csv_files <- list.files(data_folder, pattern = ".csv$", full.names = TRUE, recursive = TRUE)
+
+# Read the data in a named list
+df_luq <- setNames(map(csv_files, read_csv), basename(csv_files))
+
+
+### check if the attributes are identical for each sampling sites ----
+
+# list all the attributes
+attributes_luq <- setNames(map(names(df_luq), function(x){colnames(df_luq[x][[1]])}),names(df_luq))
+
+# Check if they are identical (could not find a way without a loop)
+for(ds in names(attributes_luq)) {
+  print(identical(attributes_luq[[1]], attributes_luq[ds][[1]]))
+}
+# => We are good, same data structure across the sampling sites
+
+##->-------- SHOULD DO THE SAME ON UNITS!!! ----------<-###
 
 
