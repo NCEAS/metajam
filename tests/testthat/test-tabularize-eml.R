dc56d17ccca367a46bccdb67e0ebbc74f6c2d614
Julien Brun <brun.julien@gmail.com>
2020-09-15 14:33:24 -0700

    adding file type test

--- tests/testthat/test-tabularize-eml.R
+++ tests/testthat/test-tabularize-eml.R
@@ -13,3 +13,9 @@ test_that("parse full table correctly", {
   metadata <- tabularize_eml(eml, full = TRUE)
   expect_equal(dim(metadata), c(189,2))
 })
+
+test_that("test fail on non xml file", {
+  eml <- system.file("extdata", "test_data", "SoilMois2012_2017.csv",
+                     package = "metajam")
+  expect_error(tabularize_eml(eml, full = TRUE))
+})
