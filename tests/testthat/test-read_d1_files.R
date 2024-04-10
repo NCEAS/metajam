ba8b02c3fa0c329d877ecfb441830e3cf4c0f446
njlyon0 <nicholasjlyon@gmail.com>
2024-04-10 14:48:11 -0400

    Tweaked `read_d1_files` tests to reflect new status of function

--- tests/testthat/test-read_d1_files.R
+++ tests/testthat/test-read_d1_files.R
@@ -14,13 +14,13 @@ test_that("test read in csv files", {
   pkg <- read_d1_files(system.file("extdata/test_data", package = "metajam"))
   names <- names(pkg)
 
-  expect_true("attribute_metadata" %in% names)
-  expect_true("summary_metadata" %in% names)
-  expect_true("data" %in% names)
-
-  expect_true(is.data.frame(pkg$attribute_metadata))
-  expect_true(is.data.frame(pkg$summary_metadata))
-  expect_true(is.data.frame(pkg$data))
+  expect_true(any(grepl(pattern = "attribute_metadata", x = names)))
+  expect_true(any(grepl(pattern = "summary_metadata", x = names)))
+  # expect_true("data" %in% names)
+#
+#   expect_true(is.data.frame(pkg$attribute_metadata))
+#   expect_true(is.data.frame(pkg$summary_metadata))
+#   expect_true(is.data.frame(pkg$data))
 })
 
 test_that("read Arctic Data Center data package", {
@@ -28,26 +28,18 @@ test_that("read Arctic Data Center data package", {
   skip_on_cran()
 
   # Call download_di_data_pkg() for data package
-  paths <- suppressMessages(
-    download_d1_data_pkg(
-      meta_obj = 'doi:10.18739/A2DP3X',
-      path = tempdir()
-    )
-  )
+  temp_dir <- tempdir()
+  paths <- download_d1_data_pkg(meta_obj = 'doi:10.18739/A2B27PS44', path = temp_dir)
+
   # Read data and metadata
-  output <- suppressMessages(
-    read_d1_files(
-      folder_path = paths[[2]]
-    )
-  )
+  output <- suppressMessages( read_d1_files(folder_path = paths[[2]]) )
+
   # Test output class
   expect_true(class(output) == 'list')
   # Test output list object names
-  expect_true(
-    all(
-      names(output) %in% c('attribute_metadata', 'summary_metadata', 'data', 'factor_metadata')
-    )
-  )
+  expect_true(any(grepl(pattern = "attribute_metadata", x = names(output))))
+  expect_true(any(grepl(pattern = "summary_metadata", x = names(output))))
+
   # Clean up
   unlink(paths, recursive = TRUE)
 })
@@ -61,23 +53,18 @@ test_that("read EDI data package", {
   paths <- suppressMessages(
     download_d1_data_pkg(
       meta_obj = 'doi:10.6073/pasta/9f2f89e48f9e943f7125d1a335d96eb0',
-      path = tempdir()
-    )
+      path = tempdir() )
   )
   # Read data and metadata
-  output <- suppressMessages(
-    read_d1_files(
-      folder_path = paths[[1]]
-    )
-  )
+  output <- suppressMessages( read_d1_files(folder_path = paths[[1]]) )
+
   # Test output class
   expect_true(class(output) == 'list')
+
   # Test output list object names
-  expect_true(
-    all(
-      names(output) %in% c('attribute_metadata', 'summary_metadata', 'data')
-    )
-  )
+  expect_true(any(grepl(pattern = "attribute_metadata", x = names(output))))
+  expect_true(any(grepl(pattern = "summary_metadata", x = names(output))))
+
   # Clean up
   unlink(paths, recursive = TRUE)
 })
